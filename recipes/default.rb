12f7e6d468db32629109203231c8b14f159276b8
Daniel Wiesmann <bakongo@gmail.com>
2014-09-09 16:41:43 -0700

    fix quoting

--- recipes/default.rb
+++ recipes/default.rb
@@ -37,7 +37,7 @@ end
 
 package 'logentries'
 
-execute "le register --account-key=#{node['le']['account_key']} --hostname='#{node['le']['hostname']} --name='#{node['le']['name']}" do
+execute "le register --account-key=#{node['le']['account_key']} --hostname='#{node['le']['hostname']}' --name='#{node['le']['name']}'" do
   not_if 'le whoami'
 end
 
